49e47378a1111086951c75b22310e7b2970bf8f0
Nathan <nathanbommezijn@gmail.com>
2020-11-20 12:00:59 +0100

    [break] uglified to create new script

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,21 @@ import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}> My shitty data </h1>
-            <Link href="/funcprog">
-                <a className={styles.link}>To functional programming</a>
-            </Link>
-            <div id="barchart"> </div>
-        </div>
+    return ( <
+        div className = {
+            styles.container
+        } >
+        <
+        h1 className = {
+            styles.title
+        } > My shitty data < /h1> <
+        Link href = "/funcprog" >
+        <
+        a className = {
+            styles.link
+        } > To functional programming < /a> <
+        /Link> <
+        div id = "barchart" > < /div> <
+        /div>
     );
 }
